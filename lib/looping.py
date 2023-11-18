4a030e02734a22e130da11054a8a390a2449df3f
matthew <shaggysci3@gmail.com>
2023-11-17 19:16:35 -0500

    fk this lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+    
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    list1 = []
+    for i in int_list:
+        list1.append(i**2)
+    return list1
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        n = fizzbuzzE(i)
+        print(n)
+
+# Write a function `fizzbuzz()` that prints the numbers from 1 to 100. For
+# multiples of three, print "Fizz" instead of the number and for the multiples
+# of five print "Buzz". For numbers which are multiples of both three and five,
+# print "FizzBuzz".
+
+def fizzbuzzE(num):
+    if num%3 == 0 and num%5 == 0: 
+      return   "FizzBuzz"
+    elif num%3 == 0:
+        return "Fizz"
+    elif num%5 ==0:
+        return "Buzz"
+    return num
